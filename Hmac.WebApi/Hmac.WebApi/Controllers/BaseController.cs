5e012222d0cb7cd0c225e3c54ab693bc076b9635
mrprofile <ccapil@yahoo.com>
2015-06-17 00:10:34 -0700

    add basecontroller

---
+++ Hmac.WebApi/Hmac.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using Hmac.WebApi.Core.ApiLibrary;
+
+namespace Hmac.WebApi.Controllers
+{
+    public class BaseController : Controller
+    {
+        private EsqtvApi _restClient;
+        protected EsqtvApi RestClient
+        {
+            get
+            {
+                return _restClient ?? (_restClient = new EsqtvApi("75a6e149019f417b92001278c7d780f3", "c7b064ebd5a64a88b40811b836374bac", "http://localhost:44388/api/"));
+            }
+            set
+            {
+                _restClient = value;
+            }
+        }
+    }
+}
--- Hmac.WebApi/Hmac.WebApi/Controllers/HomeController.cs
+++ Hmac.WebApi/Hmac.WebApi/Controllers/HomeController.cs
@@ -1,25 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.Web.Mvc;
-using Hmac.WebApi.Core;
-using Hmac.WebApi.Core.ApiLibrary;
 using RestSharp;
 
 namespace Hmac.WebApi.Controllers
 {
-    public class BaseController : Controller
-    {
-        public EsqtvApi RestClient { get; set; }
-    }
-
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
-            var client = new EsqtvApi("75a6e149019f417b92001278c7d780f3", "c7b064ebd5a64a88b40811b836374bac", "http://localhost:44388/api/");
-
             var request = new RestRequest("values", Method.GET) { RequestFormat = DataFormat.Json };
 
-            var securedResponse = client.Execute<List<string>>(request);
+            var securedResponse = RestClient.Execute<List<string>>(request);
 
             return View(securedResponse.Data);
         }
