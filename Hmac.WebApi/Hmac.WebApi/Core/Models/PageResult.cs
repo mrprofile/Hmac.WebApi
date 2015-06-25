f83157aa48cdcef93dfb7dd92110304dd969fe67
mrprofile <ccapil@yahoo.com>
2015-06-24 21:49:11 -0700

    added query base

---
+++ Hmac.WebApi/Hmac.WebApi/Core/Models/PageResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Hmac.WebApi.Core.Models
+{
+    public class PageResult<T> where T : class
+    {
+        public string Elapsed { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public int ItemCount { get; set; }
+        public int CurrentPage { get; set; }
+
+        public T Result { get; set; }
+        public dynamic Facets { get; set; }
+    }
+}
---
+++ Hmac.WebApi/Hmac.WebApi/Core/Models/QueryBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Hmac.WebApi.Core.Models
+{
+    public class QueryBase
+    {
+        public string Query { get; set; }
+        public int Limit { get; set; }
+        public int Offset { get; set; }
+        public string Order { get; set; }
+        public string Sort { get; set; }
+    }
+}
