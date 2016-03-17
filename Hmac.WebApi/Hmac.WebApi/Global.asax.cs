b552a67f3a0416892fc2dabac181df1f59f7b161
mrprofile <ccapil@yahoo.com>
2016-03-17 12:59:45 -0700

    Update Global.asax.cs

--- Hmac.WebApi/Hmac.WebApi/Global.asax.cs
+++ Hmac.WebApi/Hmac.WebApi/Global.asax.cs
@@ -17,6 +17,7 @@ namespace Hmac.WebApi
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
         }
     }
 }
